624400029583c230329f1954670d4b10867868f3
Kevin Palis <kdp44@cornell.edu>
2022-02-21 00:38:14 -0500

    Started adding some validations as spark do not support real table constraints.

--- scout/src/link_scout.py
+++ scout/src/link_scout.py
@@ -17,7 +17,15 @@ def main(argv):
     exitCode = ReturnCodes.SUCCESS
     #initialize spark session
     spark = SparkSession.builder.master("local[*]").appName("LinkScout").getOrCreate()
-    # Define schema
+
+    path = "../input/part1_in.json"
+    res = loadPersonData(spark, path, isVerbose)
+    if res is not ReturnCodes.SUCCESS:
+        exitWithException(res, spark)
+
+def loadPersonData(spark, filePath, isVerbose):
+
+     # Define schema
     schema = StructType([
         StructField("id",LongType(),False),
         StructField("first",StringType(),False),
@@ -30,35 +38,36 @@ def main(argv):
             StructField('end', DateType(), True)
             ])))
     ])
-    path = "../input/part1_in.json"
-    res = loadPersonData(spark, schema, path, isVerbose)
-    if res is not ReturnCodes.SUCCESS:
-        exitWithException(res, spark)
-
-def loadPersonData(spark, schema, filePath, isVerbose):
-     # Read json data to dataframe
+    # Read json data to dataframe
     df1 = spark.read.option("multiline","true").schema(schema).json(filePath)
     if isVerbose: 
         df1.show()  
         df1.printSchema()
     # Creates a temporary view using the DataFrame
     df1.createOrReplaceTempView("person")
-    query = spark.sql("SELECT first, last, experience.company, experience.start FROM person WHERE phone IS NOT null")
+    query = spark.sql("select first, last, experience.company, experience.start FROM person WHERE phone IS NOT null")
     if isVerbose:
         query.show()
     #validations
-
+    idDups = spark.sql("select count(*) - count(distinct id) as ctr from person").first()
+    #d = query.rdd.map(lambda p: p.dups).collect()
+    print (idDups)
+    print (type(idDups.ctr))
+    if idDups.ctr > 0:
+        return ReturnCodes.ID_DUPLICATED
     #return ReturnCodes.INCOMPLETE_PARAMETERS #test
-    return ReturnCodes.SUCCESS
+    else: 
+        return ReturnCodes.SUCCESS
 
 def exitWithException(eCode, spark):
-	try:
-		spark.stop()
-		raise LSException(eCode)
-	except LSException as e1:
-		LSUtility.printError(e1.message)
-		#traceback.print_exc()
-		sys.exit(eCode)
+    try:
+        spark.stop()
+        raise LSException(eCode)
+    except LSException as e1:
+        print("Error code: %s" % e1.code)
+        LSUtility.printError(e1.message)
+        #traceback.print_exc()
+        sys.exit(eCode)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
--- scout/src/util/ls_utility.py
+++ scout/src/util/ls_utility.py
@@ -22,6 +22,7 @@ class ReturnCodes:
 	FEATURE_NOT_IMPLEMENTED = 8
 	NO_PATH_FOUND = 9
 	NO_FILTERS_APPLIED_TO_TARGET = 10
+	ID_DUPLICATED = 11
 	MESSAGES = {
 		SUCCESS: "Operation completed successfully.",
 		INCOMPLETE_PARAMETERS: "There were fewer parameters passed than what is required. Please check the usage help (-h).",
@@ -33,7 +34,8 @@ class ReturnCodes:
 		OUTPUT_FILE_CREATION_FAILED: "Creating the output file failed.",
 		FEATURE_NOT_IMPLEMENTED: "This feature is not implemented for this version of GQL.",
 		NO_PATH_FOUND: "No path can be derived between the two vertices given. Both direct descendants and common relative algorithms have been exhausted.",
-		NO_FILTERS_APPLIED_TO_TARGET: "The filters selected did not reduce a non-entry vertex. Aborting to avoid a potentially huge query."
+		NO_FILTERS_APPLIED_TO_TARGET: "The filters selected did not reduce a non-entry vertex. Aborting to avoid a potentially huge query.",
+		ID_DUPLICATED: "A duplicate ID was found."
 	}
 
 
